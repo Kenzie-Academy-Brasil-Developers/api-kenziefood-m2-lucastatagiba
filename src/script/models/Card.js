b3860c7b45a34530fed46eceb884afa3b897dc44
lucastatagiba <marlucetata@hotmail.com>
2022-01-24 20:33:14 -0300

    adicionando constructor e metodo de criaçao

--- src/script/models/Card.js
+++ src/script/models/Card.js
@@ -1,6 +1,11 @@
 class Card {
-    constructor({ id, img, categoryIcon, category, name, description, price }) {
-
+    constructor({ id, imagem, categoria, nome, descricao, preco }) {
+        this.id = id
+        this.img = imagem
+        this.category = categoria
+        this.name = nome
+        this.description = descricao
+        this.price = preco
     }
 
 
@@ -10,7 +15,37 @@ class Card {
 
     // cria cada card
     createElement() {
+        const showCase = document.querySelector('#panel__showCase')
+        const articleCard = document.createElement('article')
+        articleCard.classList.add = 'articleCard'
 
+        const imgCard = document.createElement('img')
+        imgCard.classList.add = 'productImg'
+        imgCard.src = this.img
+
+        const category = document.createElement('span')
+        category.classList.add = 'category'
+        category.classList.add = this.category
+        category.innerText = this.category
+
+        const name = document.createElement('span')
+        name.classList.add = 'name'
+        name.innerText = this.name
+
+
+        const description = document.createElement('p')
+        description.classList.add = 'description'
+        description.innerText = this.description
+
+        const price = document.createElement('span')
+        price.classList.add = 'price'
+        price.innerText = this.price
+
+        const iconAddCart = document.createElement('img')
+        iconAddCart.classList.add = 'iconAddCart'
+
+        showCase.appendChild(articleCard)
+        articleCard.append(imgCard, category, name, description, price, iconAddCart)        
     }
 
     // evento para chamar outros eventos
