ffc170fbe045c55a3acc55148a318c723cd3ab6d
jrpolesi <josericardo.kremer@gmail.com>
2022-01-24 21:43:08 -0300

    Adicionar metodos restantes do Fetch

--- src/script/models/Fetch.js
+++ src/script/models/Fetch.js
@@ -1,22 +1,57 @@
 class Fetch {
+    static #url = 'https://kenzie-food-api.herokuapp.com'
+    static #token = 'uigdlihgfiaf8afhe9i7fg9iafh9ai'
 
-    static async get() {
-        const res = await fetch('https://kenzie-food-api.herokuapp.com/product')
+    static async get(endpoint) {
+        const res = await fetch(Fetch.#url + endpoint)
+
         const data = await res.json()
+
         return data
     }
 
-    static post() {
+    static post(endpoint, body) {
+        const res = await fetch(Fetch.#url + endpoint, {
+            method: 'POST',
+            headers: {
+                Authorization: Fetch.#token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        })
 
+        const data = await res.json()
+
+        return data
     }
 
-    static patch() {
+    static patch(endpoint, body) {
+        const res = await fetch(Fetch.#url + endpoint, {
+            method: 'PATCH',
+            headers: {
+                Authorization: Fetch.#token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        })
 
+        const data = await res.json()
+
+        return data
     }
 
-    static delete() {
+    static delete(endpoint,) {
+        const res = await fetch(Fetch.#url + endpoint, {
+            method: 'DELETE',
+            headers: {
+                Authorization: Fetch.#token,
+            }
+        })
 
+        const data = await res.json()
+
+        return data
     }
 }
 
-export {Fetch}
+export { Fetch }
